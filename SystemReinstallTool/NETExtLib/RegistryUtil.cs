dbb52071fe14e4fe74f5e3777e67759a10df3ac8
Henry.T <tang.hy@live.com>
2017-05-30 18:59:04 +0800

    add SystemReinstallTool

---
+++ SystemReinstallTool/NETExtLib/RegistryUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETExtLib
+{
+    public class RegistryUtil
+    {
+        public static void Export(string exportPath, string registryPath)
+        { 
+            string path = "\""+ exportPath + "\"";
+            string key = "\""+ registryPath + "\"";
+            Util.StartProcessToEnd("regedit.exe", "/e " + path + " " + key + " ");
+        }
+    }
+}
